04d1a1feb3f6e32ebd76370d4bdd6770c631465d
Nari07 <mickinguyen12@gmail.com>
2023-01-17 22:04:33 -0600

    add ui and remove console.log parts

--- script.js
+++ script.js
@@ -1,82 +1,165 @@
 //computer's random choice
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['squirtle', 'charmander', 'bulbasaur'];
 const randChoice = () => Math.floor(Math.random() * choices.length);
-const getComputerChoice = () => choices[(randChoice())];
-const computerSelection = getComputerChoice();
+let getComputerChoice = () => choices[(randChoice())];
 
-//player's input/choice
-const playerSelection = getPlayerChoice()
-function getPlayerChoice() {
-      let choice = prompt("Make your selection: Rock, Paper, Scissors")
-  return choice.toLowerCase()  
-}
-
-//Goal = console.log() to display the results of each round and the winner at the end.
 //5 round game that keeps score and reports a winner or loser at the end
 
-let win;
-let loss;
-let tie;
+let computerScore = 0
+let playerScore = 0
 
- let computerScore = 0
- let playerScore = 0
+let choice = document.getElementById('choices');
+let result = document.getElementById('result');
+let displayWin = document.getElementById('displayWinner');
+let squirtle = document.getElementById('squirtle');
+let bulb = document.getElementById('bulbasaur');
+let char = document.getElementById('charmander');
+let rem1 = document.getElementById('remove1');
+let rem2 = document.getElementById('remove2');
+let rem3 = document.getElementById('remove3');
+let btns = document.getElementById('btns');
 
-function playRound(playerSelection, computerSelection) {
-  console.log('Your choice is ' + playerSelection + '.' + ' Computer\'s choice is ' + computerSelection + '.');
+function disable(){
+  //  squirtle.style.visibility='hidden';
+  //  bulb.style.visibility='hidden';
+  //  char.style.visibility='hidden';
+  result.textContent ='';
+  choice.textContent = '';
+  bulb.remove();
+  squirtle.remove();
+  char.remove();
+  let snor = document.createElement('p');
+  snor.setAttribute('id', 'snorlax');
+  snor.innerHTML = ('<img src = "https://cdn.donmai.us/original/d2/d3/d2d3b295643cbb1a8948dc23ee3d9ae1.gif"/>');
+  rem1.appendChild(snor);
+   
+}
+
+function enable(){
+  //  squirtle.style.visibility = 'visible';
+  //  bulb.style.visibility = 'visible';
+  //  char.style.visibility = 'visible';
+  rem1.appendChild(squirtle);
+  rem2.appendChild(bulb);
+  rem3.appendChild(char);
+  let snorToRemove = document.getElementById('snorlax');
+  snorToRemove.remove();
+}
+
+function playRound(e){
+   let playerSelection =  event.target.id;
+   let computerSelection = getComputerChoice();
+
+   choice.textContent = 'Computer\'s choice is ' + computerSelection + ' .';
  
-  if (computerSelection === playerSelection) {
-      console.log('It\'s a tie!');
+   if (computerSelection === playerSelection) {
+     result.textContent = ('It\'s a tie!');
      
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-      console.log('You lose! Paper beats rock!!');
+  } else if (playerSelection === 'squirtle' && computerSelection === 'bulbasaur'){
+      result.textContent = ('You lose! Grass beats Water!!');
       computerScore++;
      
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-     console.log ('You win! Rock beats scissors!');
-     playerScore++;
+  } else if (playerSelection === 'squirtle' && computerSelection === 'charmander'){
+      result.textContent = ('You win! Water beats Fire!');
+      playerScore++;
    
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-     console.log ('You win! Paper beats rock!!');
-     playerScore++;
+  } else if (playerSelection === 'bulbasaur' && computerSelection === 'squirtle'){
+      result.textContent = ('You win! Grass beats Water!!');
+      playerScore++;
    
-  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-     console.log ('You lose! Scissors beats paper');
-     computerScore++;
+  } else if (playerSelection === 'bulbasaur' && computerSelection === 'charmander'){
+      result.textContent = ('You lose! Fire beats Grass');
+      computerScore++;
      
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-     console.log ('You win! Scissors beats paper!');
-     playerScore++;
+  } else if (playerSelection === 'charmander' && computerSelection === 'bulbasaur'){
+      result.textContent = ('You win! Fire beats Grass!');
+      playerScore++;
    
-  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-     console.log ('You lose! Rock beats scissors!')
-     computerScore++;
+  } else if (playerSelection === 'charmander' && computerSelection === 'squirtle'){
+      result.textContent = ('You lose! Water beats Fire!')
+      computerScore++;
    
   } else {
-   console.log ('Invalid choice! Please choose again.')
-   }
-   let score = console.log('Your current score is ' + playerScore + '. Computer\'s current score is ' + computerScore +   '.');
-   
-}
- 
-let determineWinner = () => {
-      if (playerScore > computerScore) {
-      console.log('Game over. You are the winner!! Congrats!')
+      result.textContent = ('Invalid choice! Please choose again.');
   }
-  else if (playerScore < computerScore) {
-      console.log ('Game over. You lose!!! Sucks!!')
-  }
-  else if (playerScore == computerScore) {
-      console.log ('Game over. It\'s a tie!! Play again?')
-  }
-}
-
-//5 rounds
-function game() {
-  for (let i = 1; i < 6; i++) {
-    console.log('Round '+ i)
-    playRound(getPlayerChoice(), getComputerChoice());
-  }
+  
+  document.getElementById('yourScore').textContent = playerScore;
+  document.getElementById('compScore').textContent = computerScore;
+  
+  let determineWinner = () => {
+     if (playerScore > computerScore && playerScore >= 5) {
+        displayWin.textContent = ('Game over. You win!! Congrats!')
+       	disable();
+        let di = document.createElement('p');
+        di.innerHTML = ('Try again?');
+        displayWin.appendChild(di);
+        let butt = document.createElement('button');
+        let done = document.createElement('p');
+        done.appendChild(butt)
+        done.setAttribute('id', 'hate');
+        butt.innerHTML = ('New Game');
+        butt.setAttribute('id', 'tryAg');
+        displayWin.appendChild(done);
+        let but2 = document.getElementById('tryAg')
+        but2.addEventListener('click', function(){
+          enable();
+          computerScore = 0;
+          playerScore = 0;
+          playRound(e);
+          displayWin.removeChild(di);
+          displayWin.textContent = '';
+          result.textContent ='Choose a Pokémon!';
+          choice.textContent = '';
+     		})
+     } else if (playerScore < computerScore && computerScore >= 5) {
+        displayWin.textContent = ('Game over. You lose!!! Sucks!!')
+        disable();
+        let d = document.createElement('p');
+        d.innerHTML = ('Try again?');
+        displayWin.appendChild(d);
+        let butt = document.createElement('button');
+        let done = document.createElement('p');
+        done.appendChild(butt)
+        done.setAttribute('id', 'hate');
+        butt.innerHTML = ('New Game');
+        butt.setAttribute('id', 'tryAg');
+        displayWin.appendChild(done);
+        let but2 = document.getElementById('tryAg')
+        but2.addEventListener('click', function(){
+          enable();
+          computerScore = 0;
+          playerScore = 0;
+          playRound(e);
+          displayWin.removeChild(d);
+          displayWin.textContent = '';
+					result.textContent ='Choose a Pokémon!';
+          choice.textContent = '';
+     		})
+     	}
+	}
+  
   determineWinner();
 }
+ 
+document.getElementById('squirtle').addEventListener('click', playRound);
+document.getElementById('bulbasaur').addEventListener('click', playRound);
+document.getElementById('charmander').addEventListener('click', playRound);
 
-game();
+
+
+
+//5 rounds
+/* function game(){
+  for (let i = 1; i < 5; i++) {
+   let round = result.document.createTextNode('Round '+ i);
+   result.appendChild(round);
+   playRound(getPlayerChoice(), getComputerChoice());
+     }
+  determineWinner();
+  }
+} */
+
+/* function endGame(){
+  if 
+} */
+
